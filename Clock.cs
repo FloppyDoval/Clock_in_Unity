fa3810dd8c27efc357dfaeac735c3ee4d2721a96
Florencia Doval <florencia_doval@brown.edu>
2023-09-30 20:05:11 -0400

    Add files via upload

---
+++ Clock.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+	[SerializeField]
+	Transform hoursPivot, minutesPivot, secondsPivot;
+
+	void Update () {
+		var time = DateTime.Now;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+	}
+}
